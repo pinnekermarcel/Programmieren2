65289a7f64c81403f25fa4a30ac00e46352d37a7
pinnekermarcel <pinnekermarcel@gmail.com>
2026-05-04 15:27:43 +0200

    lektion18 üB1

---
+++ main/java/lektion18/Üb1/MatrikelNummerException.java
@@ -0,0 +1,7 @@
+package lektion18.Üb1;
+
+public class MatrikelNummerException extends Exception {
+    public MatrikelNummerException(String mesasge) {
+        super(mesasge);
+    }
+}
---
+++ main/java/lektion18/Üb1/Studiengangssplitter.java
@@ -0,0 +1,49 @@
+package lektion18.Üb1;
+
+import java.io.*;
+
+public class Studiengangssplitter {
+
+    public void splitStudiengaenge(String dateiname) throws IOException, MatrikelNummerException {
+        try(
+                //1 reader
+            BufferedReader br = new BufferedReader(new FileReader(dateiname));
+
+            BufferedWriter writerInf = new BufferedWriter(new FileWriter("InfNr.txt"));
+            BufferedWriter writerEC = new BufferedWriter(new FileWriter("ECNr.txt"));
+            BufferedWriter writerWInf = new BufferedWriter(new FileWriter("WInfNr.txt"))
+            ){
+            String zeile;
+            while((zeile = br.readLine()) != null){
+                int nummer = Integer.parseInt(zeile);
+                if(nummer >= 5000000 && nummer <= 5099999 ){
+                    writerWInf.write(zeile);
+                    writerWInf.newLine();
+                }else if(nummer >= 5100000 && nummer <= 5199999 ){
+                    writerInf.write(zeile);
+                    writerInf.newLine();
+                }else if(nummer >= 6100000 && nummer <= 6199999 ){
+                    writerEC.write(zeile);
+                    writerEC.newLine();
+                }else{
+                    throw new MatrikelNummerException("Ungültige Matrikelnummer! " + zeile);
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+
+        Studiengangssplitter studiengangssplitter = new Studiengangssplitter();
+
+        try{
+            studiengangssplitter.splitStudiengaenge("MatrNr.txt");
+            System.out.println("Aufteilung war erfolgreich!");
+
+        }catch(MatrikelNummerException e){
+            System.err.println("Ungültige Matrikelnummer!" +  e.getMessage());
+
+        } catch (IOException e) {
+            System.err.println("Probleme mit den Dateien! " + e.getMessage());
+        }
+    }
+}
